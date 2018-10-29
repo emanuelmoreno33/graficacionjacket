c23a1229f091d100acc27b92f3adc7a06ca9b215
emanuelmoreno33 <emanuel.moreno16@tectijuana.edu.mx>
2018-10-29 11:18:23 -0700

    cambios en degradado

--- src/graficacion/jacket/GraficacionJacket.java
+++ src/graficacion/jacket/GraficacionJacket.java
@@ -1,12 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package graficacion.jacket;
-import javax.swing.JApplet;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.*;
+import javax.swing.JFrame;
 
-
-public class GraficacionJacket extends JApplet{
-
-    public void init() {       
-        Paneldegradado pd = new Paneldegradado();
-        add(pd);
+/**
+ *
+ * @author Maxcel Center
+ */
+public class GraficacionJacket extends JFrame{
+    public static void main(String[] args)
+    {
+        ventana window = new ventana();
+        
+        window.setTitle("Primera ventana");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        degradadolineal degradado = new degradadolineal();
+        degradado.setBounds(0, 0, 400, 600);
+        window.getContentPane().add(degradado);
+        window.setVisible(true);
+        window.setSize(400,600);
+        
     }
-    
 }
--- src/graficacion/jacket/degradadolineal.java
+++ src/graficacion/jacket/degradadolineal.java
@@ -1,23 +1,19 @@
-package Graficacion.jacket;
+package graficacion.jacket;
 import java.awt.Color;
 import javax.swing.JPanel;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-/**
- *
- * @author Usuario
- */
-class degradadoHorizontal extends JPanel {
 
-    public degradadoHorizontal() {
+public class degradadolineal extends JPanel {
+    public degradadolineal() {
     }
     public void paint(Graphics g){
         Graphics2D g2d=(Graphics2D)g;
         //Creo el degradado horizontal con las
         //coordenadas (0,0) al (anchura del componente, 0)
-        GradientPaint horizontalGradient = new GradientPaint (0,0, Color.RED, getWidth(),0,Color.BLUE);
+        GradientPaint horizontalGradient = new GradientPaint (200,0, Color.BLUE, 200,400,Color.PINK);
         g2d.setPaint(horizontalGradient);
-        g2d.fillRect(0,0, getWidth(), getHeight());
+        g2d.fillRect(0,0, 400, 600);
     }
 }
